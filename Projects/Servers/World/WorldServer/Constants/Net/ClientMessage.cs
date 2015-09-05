063ffd8104ea86abbe189a3295190bb0f2131cde
Nexvelius <nexvelius@gmail.com>
2015-09-05 21:35:00 +0300

    gag [QueuedMessagesEnd]

--- Projects/Servers/World/WorldServer/Constants/Net/ClientMessage.cs
+++ Projects/Servers/World/WorldServer/Constants/Net/ClientMessage.cs
@@ -16,6 +16,7 @@ namespace WorldServer.Constants.Net
         #endregion
 
         #region UserClient
+            QueuedMessagesEnd = 0x0130,
         #endregion
     }
 }
---
+++ Projects/Servers/World/WorldServer/Packets/Client/Character/QueuedMessagesEnd.cs
@@ -0,0 +1,15 @@
+﻿using Framework.Network.Packets;
+
+namespace WorldServer.Packets.Client.Character
+{
+
+    public class QueuedMessagesEnd : ClientPacket
+    {
+        public int Timestamp { get; set; }
+
+        public override void Read()
+        {
+            Timestamp = Packet.Read<int>();
+        }
+    }
+}
